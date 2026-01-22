6f0e08813a9574a9828d6e0b67f7c2a9fb7931a0
ahmed-Lutfi <142930077+ahmed-Lutfi@users.noreply.github.com>
2026-01-22 21:33:11 +0300

    Update settings.py

--- mini_edr/settings.py
+++ mini_edr/settings.py
@@ -11,7 +11,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+# 1. This is where your files ARE right now
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'web', 'static'),
+]
 
+# 2. This is where they will GO when you deploy (Render uses this)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# 3. This is the URL used in HTML
+STATIC_URL = '/static/'
+
+# 4. Use WhiteNoise to serve them
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_URL = 'static/'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
